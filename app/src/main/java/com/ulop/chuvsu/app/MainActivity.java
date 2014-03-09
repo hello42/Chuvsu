9685c83171648c74fcf222bed85b2bfb2b11b806
Ulop <UlopLT@gmail.com>
2014-03-09 16:11:46 +0400

    NewsCardFragment changed

--- app/src/main/java/com/ulop/chuvsu/app/MainActivity.java
+++ app/src/main/java/com/ulop/chuvsu/app/MainActivity.java
@@ -5,17 +5,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.ulop.newscardlist.dummy.NewsCardFragment;
@@ -53,7 +49,7 @@ public class MainActivity extends ActionBarActivity
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.container, NewsCardFragment.newInstance("lol","lol"))
+                .replace(R.id.container, NewsCardFragment.newInstance("lol", "lol"))
                 .commit();
     }
 
--- app/src/main/java/com/ulop/newscardlist/dummy/NewsCardFragment.java
+++ app/src/main/java/com/ulop/newscardlist/dummy/NewsCardFragment.java
@@ -1,21 +1,25 @@
 package com.ulop.newscardlist.dummy;
 
-import android.app.Activity;
+
+
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.ulop.chuvsu.app.R;
 
 /**
- * A fragment representing a list of Items.
- * <p />
- * <p />
- * Activities containing this fragment MUST implement the {@link Callbacks}
- * interface.
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ * Use the {@link com.ulop.newscardlist.dummy.NewsCardFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
  */
-public class NewsCardFragment extends ListFragment {
-
+public class NewsCardFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -25,9 +29,16 @@ public class NewsCardFragment extends ListFragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
 
-    // TODO: Rename and change types of parameters
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NewsCardFragment.
+     */
+    // TODO: Rename and change types and number of parameters
     public static NewsCardFragment newInstance(String param1, String param2) {
         NewsCardFragment fragment = new NewsCardFragment();
         Bundle args = new Bundle();
@@ -36,70 +47,36 @@ public class NewsCardFragment extends ListFragment {
         fragment.setArguments(args);
         return fragment;
     }
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public NewsCardFragment() {
+        // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-        // TODO: Change Adapter to display your content
-        setListAdapter(new NewsCardAdapter(getActivity()));
-    }
-
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.list_of_news, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listView1);
+        NewsCardAdapter cardAdapter = new NewsCardAdapter(rootView.getContext());
+        listView.setAdapter(cardAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), NewsCardAdapter.objects.get(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
     }
 
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        if (null != mListener) {
-            // Notify the active callbacks interface (the activity, if the
-            // fragment is attached to one) that an item has been selected.
-            //mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).title);
-        }
-    }
-
-    /**
-    * This interface must be implemented by activities that contain this
-    * fragment to allow an interaction in this fragment to be communicated
-    * to the activity and potentially other fragments contained in that
-    * activity.
-    * <p>
-    * See the Android Training lesson <a href=
-    * "http://developer.android.com/training/basics/fragments/communicating.html"
-    * >Communicating with Other Fragments</a> for more information.
-    */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(String id);
-    }
 
 }
